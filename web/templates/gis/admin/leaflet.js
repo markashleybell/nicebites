16ceacecf98b6b31290aa8cf6d9d2b93ea6725c0
Mark Bell <me@markashleybell.com>
2013-03-22 07:57:49 +0000

    Establishment admin postcode location, auto slug

--- web/templates/gis/admin/leaflet.js
+++ web/templates/gis/admin/leaflet.js
@@ -9,30 +9,64 @@
                      '<a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 
 {{ module }}.tiles = null;
+{{ module }}.marker = null;
+{{ module }}.lat = '50.37027608930287';
+{{ module }}.lng = '-4.142275169805998';
 {% endblock %}
+
+{{ module }}.locateByPostcode = function(_postcode) {
+
+    var coords;
+
+    _postcode = django.jQuery.trim(_postcode.replace(/\s+/gi, '')).toUpperCase();
+
+    if (!_postcode.match(/^\b([A-Z]{1,2}[0-9][A-Z0-9]?)[\s+]?([0-9][ABD-HJLNP-UW-Z]{2})\b$/i))
+    {
+        alert('You must enter a valid postcode.');
+        return;
+    }
+
+    django.jQuery.ajax({
+        url: 'http://markashleybell.iriscouch.com/os_data/_design/data/_view/bypostcode?key=%22' + _postcode + '%22',
+        dataType: 'jsonp',
+        success: function (data, status, request) {
+
+            var row = data.rows[0].value;
+
+            coords = new L.LatLng(row.lat, row.lng);
+
+            {{ module }}.map.panTo(coords);
+
+            {{ module }}.marker.setLatLng(coords);
+
+            django.jQuery('#{{ id }}').val('SRID=4326;POINT (' + coords.lng + ' ' + coords.lat + ')');
+
+        },
+        error: function (request, status, error) { }
+    });
+
+};
+
 {{ module }}.init = function(){
 
     {% if not is_point %}
     alert('Only point data is supported');
     {% endif %}
 
-    var _lon = '-4.142275169805998';
-    var _lat = '50.37027608930287';
-    
     var pointRegex = /POINT \(([\-.0-9]+) ([\-.0-9]+)\)/;
 var match = pointRegex.exec(django.jQuery('#{{ id }}').val());
 if (match == null) {
     // alert('Invalid POINT');
 } else {
-    _lat = match[2];
-    _lon = match[1];
+    {{ module }}.lat = match[2];
+    {{ module }}.lng = match[1];
 }
 
     // The admin map for this geometry field.
     {% block map_creation %}
     {{ module }}.map = L.map('{{ id }}_map');
     {{ module }}.tiles = new L.TileLayer({{ module }}.tileUrl, {minZoom: 8, maxZoom: 18, attribution: {{ module }}.attrib, subdomains: {{ module }}.subDomains});
-    {{ module }}.map.setView([_lat, _lon], 15);
+    {{ module }}.map.setView([{{ module }}.lat, {{ module }}.lng], 15);
     {{ module }}.map.addLayer({{ module }}.tiles);
 
     var _icon = new L.Icon({
@@ -44,16 +78,16 @@ if (match == null) {
                     shadowSize: new L.Point(41, 41)
                 });
 
-        var _position = new L.LatLng(_lat, _lon);
-                    var _marker = new L.Marker(_position, { icon: _icon, draggable: true });
-                    {{ module }}.map.addLayer(_marker);
+                    var _position = new L.LatLng({{ module }}.lat, {{ module }}.lng);
+                    {{ module }}.marker = new L.Marker(_position, { icon: _icon, draggable: true });
+                    {{ module }}.map.addLayer({{ module }}.marker);
 
-                    _marker.on('dragend', function (e) {
+                    {{ module }}.marker.on('dragend', function (e) {
                      var coords = e.target.getLatLng();
-                     _lat = coords.lat;
-                     _lon = coords.lng;
+                     {{ module }}.lat = coords.lat;
+                     {{ module }}.lng = coords.lng;
                      // alert(_lat + ':' + _lng);
-                     django.jQuery('#{{ id }}').val('SRID=4326;POINT (' + _lon + ' ' + _lat + ')');
+                     django.jQuery('#{{ id }}').val('SRID=4326;POINT (' + {{ module }}.lng + ' ' + {{ module }}.lat + ')');
                     });
 //marker.bindPopup('<h1>' + item.name + '</h1>' + item.postcode);
 // _markers.push(marker);
