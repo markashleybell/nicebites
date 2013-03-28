f1dcbce0726187191210765a31561fc85a2a8876
Mark Bell <me@markashleybell.com>
2013-03-28 08:00:47 +0000

    Temp home page script

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,10 +1,9 @@
 $(function () {
 
-    // Bind the distance buttons in the header bar
-    $('#distance-selector button').on('click', function(evt) {
-        evt.preventDefault();
-        NiceBites.loadMarkersWithin($(this).data('distance'));
-    });
+    var clientPosition = {
+        lat: '50.36833260763417',
+        lng: '-4.140386894659514'
+    };
 
     //_mapContainer = $('#map-container');
 
@@ -15,5 +14,54 @@ $(function () {
 
     // $(window).trigger('resize');
 
-    NiceBites.init('map', 1);
+    // Initially load the map centred on the region
+    NiceBites.init('map');
+    NiceBites.positionMap({
+        position: clientPosition,
+        zoom: 15,
+        callback: function() { }
+    });
+    NiceBites.loadMarkers({
+        position: clientPosition,
+        zoom: 15,
+        range: 5,
+        regionId: 1,
+        callback: function() {
+            NiceBites.getClientPosition(function(pos) {
+                clientPosition = pos;
+                NiceBites.positionMap({
+                    position: clientPosition,
+                    zoom: 15,
+                    callback: function() { }
+                });
+                NiceBites.loadMarkers({
+                    position: pos,
+                    zoom: 15,
+                    range: 5,
+                    regionId: 1,
+                    callback: function() { 
+                        var log = NiceBites.getLog();
+                        for(var i=0; i<log.length; i++)
+                            console.log(log[i]);
+                    }
+                });
+            });
+        }
+    });
+    
+    // Bind the range buttons
+    $('#distance-selector button').on('click', function(evt) {
+        evt.preventDefault();
+        var r = $(this).data('distance');
+
+        NiceBites.loadMarkers({ 
+            position: clientPosition, 
+            zoom: 15,
+            range: r,
+            regionId: 1,
+            callback: function() {
+                console.log('Adjusted range to ' + r + 'km');
+            }
+        });
+    });
 });
